f5c24fbc505065cca20bd635c75a092ff8313a6d
kumarpit <kumar.arpit77@yahoo.com>
2022-01-15 23:11:42 -0800

    capture + crop implemented

--- lib/home.dart
+++ lib/home.dart
@@ -1,15 +1,59 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  File? _imageFile = null;
+
+  _captureImage() async {
+    ImagePicker _picker = ImagePicker();
+    XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _imageFile = File(photo!.path);
+    });
+  }
+
+  Future<void> _cropImage() async {
+    File? cropped = await ImageCropper.cropImage(
+        sourcePath: _imageFile!.path,
+        aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
+        compressQuality: 100,
+        maxWidth: 700,
+        maxHeight: 700,
+        compressFormat: ImageCompressFormat.jpg,
+        androidUiSettings: AndroidUiSettings(showCropGrid: true));
+    setState(() {
+      _imageFile = cropped ?? _imageFile;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: ElevatedButton(
-      child: Text('about'),
-      onPressed: () => Navigator.pushNamed(context, '/danger'),
-    )));
+      bottomNavigationBar: BottomAppBar(
+          child: Row(
+        children: <Widget>[
+          IconButton(onPressed: _captureImage, icon: Icon(Icons.photo_camera)),
+          IconButton(onPressed: _cropImage, icon: Icon(Icons.crop))
+        ],
+      )),
+      body: ListView(
+        children: <Widget>[
+          if (_imageFile != null) ...[
+            Image.file(File(_imageFile!.path)),
+            Row(
+              children: <Widget>[
+                IconButton(onPressed: _cropImage, icon: Icon(Icons.crop))
+              ],
+            )
+          ]
+        ],
+      ),
+    );
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -5,7 +5,7 @@ import 'package:detect/results/okay.dart';
 import 'package:detect/ingredients.dart';
 
 var appRoutes = {
-  '/': (context) => const HomeScreen(),
+  '/': (context) => HomeScreen(),
   '/caution': (context) => const CautionScreen(),
   '/danger': (context) => const DangerScreen(),
   '/okay': (context) => const OkayScreen(),
